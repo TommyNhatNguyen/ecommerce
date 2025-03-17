5f7dbb9be56087e86d5214d90f60c18449e37c11
Tommy Nguyen <nguyenanhnhat123456@gmail.com>
2025-03-18 01:30:10 +0700

    feat: setup filter v1

--- client/src/app/(dashboard)/admin/(content)/inventory/components/InventoryFilter/index.tsx
+++ client/src/app/(dashboard)/admin/(content)/inventory/components/InventoryFilter/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const InventoryFilter = (props: Props) => {
   return (
-    <div>InventoryFilter</div>
-  )
-}
+    <div>
+      Tên sản phẩm (search) Theo kho hàng (select multiple) Theo trạng thái kho
+      (select multiple) Theo nhóm sản phẩm (select multiple) Theo thương hiệu
+      (select multiple) Range: giá, giá vốn trung bình, tổng số lượng tồn kho,
+      tổng giá vốn (lọc nâng cao - popup bao gồm những cái trên)
+    </div>
+  );
+};
 
-export default InventoryFilter
+export default InventoryFilter;
--- note.md
+++ note.md
@@ -3,4 +3,5 @@
 - Cho kéo thả cột (quan trọng)
 - Lưu setting cột của các bảng (quan trọng)
 - Thêm nhanh trong lúc tạo (bình thường)
+- - Thanh action follow theo bên góc phải nếu không còn trong view của user nữa (bình thường);
 - Lưu nháp (không quan trọng)
